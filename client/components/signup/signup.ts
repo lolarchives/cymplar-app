f1edb69c3b6469c3c0955be2909c057f5cf064bd
Nghia Tran <m18tran@students.latrobe.edu.au>
2015-12-15 10:55:50 +1100

    Try to initialize sign up again

---
+++ client/components/signup/signup.ts
@@ -0,0 +1,26 @@
+namespace SignUp {
+	angular.module('app.signup', [
+		'ui.router'
+	])
+	.config(config)
+	.controller('SignUpController', SignUp.SignUpController);
+	
+	/* @ngInject */
+	function config($stateProvider: any) {
+		$stateProvider
+			.state('signup', {
+				url: '/signup',
+				templateUrl: 'components/signup/signup.html',
+				controller: 'SignUpController',
+				controllerAs: 'suCtrl'
+			});
+	}
+	
+	export class SignUpController {
+		/* @ngInject */
+		constructor(private $scope: angular.IScope, private $http: angular.IHttpBackendService, private helloWorld: String) {
+			this.helloWorld = 'Hello World';
+		};
+	};
+	
+}
